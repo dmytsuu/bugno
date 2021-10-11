5ece882e28cd3ec6882d32af3270d69f7a4c8107
Dmitry Sivoglaz <grayeyed16@gmail.com>
2021-10-11 13:39:31 +0300

    Add mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,17 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: ENV['MAILER_USERNAME'],
+    password: ENV['MAILER_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+  config.action_mailer.default_url_options = { host: ENV['APPLICATION_URL'] }
+
   # Log disallowed deprecations.
   config.active_support.disallowed_deprecation = :log
 
