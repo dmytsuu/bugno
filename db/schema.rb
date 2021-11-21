1ff1a0a1b2f1beee638bc4d670d50e9fcb7a7ec4
Dmitry Sivoglaz <grayeyed16@gmail.com>
2021-11-21 22:59:05 +0200

    Drop redundant tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_06_175353) do
+ActiveRecord::Schema.define(version: 2021_11_21_205259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "active_admin_comments", force: :cascade do |t|
-    t.string "namespace"
-    t.text "body"
-    t.string "resource_type"
-    t.bigint "resource_id"
-    t.string "author_type"
-    t.bigint "author_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
-  end
 
   create_table "events", force: :cascade do |t|
     t.bigint "project_id"
@@ -58,24 +44,6 @@ ActiveRecord::Schema.define(version: 2019_12_06_175353) do
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
-  create_table "integrations", force: :cascade do |t|
-    t.bigint "project_id"
-    t.string "type"
-    t.jsonb "provider_data"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["project_id"], name: "index_integrations_on_project_id"
-  end
-
-  create_table "plans", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.integer "event_limit"
-    t.decimal "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "project_users", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "project_id"
@@ -95,19 +63,6 @@ ActiveRecord::Schema.define(version: 2019_12_06_175353) do
     t.string "slug"
     t.integer "active_event_count", default: 0, null: false
     t.index ["slug"], name: "index_projects_on_slug", unique: true
-  end
-
-  create_table "subscriptions", force: :cascade do |t|
-    t.date "expires_at"
-    t.integer "status", default: 0
-    t.integer "events", default: 0
-    t.bigint "project_id"
-    t.bigint "plan_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "customer_id"
-    t.index ["plan_id"], name: "index_subscriptions_on_plan_id"
-    t.index ["project_id"], name: "index_subscriptions_on_project_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
@@ -142,9 +97,6 @@ ActiveRecord::Schema.define(version: 2019_12_06_175353) do
 
   add_foreign_key "events", "projects"
   add_foreign_key "events", "users"
-  add_foreign_key "integrations", "projects"
   add_foreign_key "project_users", "projects"
   add_foreign_key "project_users", "users"
-  add_foreign_key "subscriptions", "plans"
-  add_foreign_key "subscriptions", "projects"
 end
