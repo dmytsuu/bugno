7c45baea8b4c05728ee22470ec3abd7503d1c6f5
Dmitry Sivoglaz <grayeyed16@gmail.com>
2021-11-21 22:29:03 +0200

    Add authorize to params controller

--- app/controllers/request_params_controller.rb
+++ app/controllers/request_params_controller.rb
@@ -1,6 +1,7 @@
 class RequestParamsController < ApplicationController
   def show
     @event = Event.find(params[:event_id])
+    authorize(@event.project)
     @request_params = [{ name: 'Headers', data: @event.headers },
                        { name: 'Params', data: @event.params },
                        { name: 'User', data: @event.person_data }]
