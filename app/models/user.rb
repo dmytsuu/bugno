29eb087b17dbc8445ec6dd4db95d4c842299a419
Dmitry Sivoglaz <grayeyed16@gmail.com>
2022-03-25 14:54:39 +0200

    add #assign_extras

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,13 @@ class User < ActiveRecord::Base
 
   has_many :project_users, dependent: :destroy
   has_many :projects, through: :project_users
+
+  before_create :assign_extras
+
+  def assign_extras
+    self.nickname = email
+    self.name = email
+    self.uid = SecureRandom.base64
+    self.provider = 'email'
+  end
 end
