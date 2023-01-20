07eff62dcaae5b9e0083437425c20ebdf1e1733f
rankore <grayeyed16@gmail.com>
2023-01-20 04:54:24 +0200

    use replace instead remove in broadcast && refactor template

--- app/controllers/event_collections_controller.rb
+++ app/controllers/event_collections_controller.rb
@@ -1,13 +1,18 @@
+# frozen_string_literal: true
+
 class EventCollectionsController < ApplicationController
   def destroy
-    project = Project.find(collection_params[:project_id])
+    project = Project.find(collection_params[:id])
     authorize(project)
     project.events.where(status: collection_params[:status]).delete_all
-    project.broadcast_remove_to project, target: "#{collection_params[:status]}_events"
+    project.broadcast_replace_to project,
+                                 target: "#{collection_params[:status]}_events",
+                                 partial: 'projects/events_collection',
+                                 locals: { project_id: project.id, status: collection_params[:status] }
     head :no_content
   end
 
   def collection_params
-    params.permit(:project_id, :status)
+    params.permit(:id, :status)
   end
 end
