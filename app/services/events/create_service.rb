0fce0b58a0688325c464944de20d6678eed3853e
rankore <grayeyed16@gmail.com>
2023-08-01 17:42:35 +0300

    upd event create condition

--- app/services/events/create_service.rb
+++ app/services/events/create_service.rb
@@ -5,7 +5,7 @@ class Events::CreateService < ApplicationService
     return event unless project
 
     # ::Events::ResolveSourceCodeService.call(event: event, trace: event.backtrace[0]) if resolve_source?
-    event.save if event.parent? || event.occurrences.count.zero?
+    event.save if event.parent? || parent.occurrence_count.zero?
     update_parrent if event.occurrence? && event.valid?
     event
   end
